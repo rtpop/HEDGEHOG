6504e1b9099268ae1878eaf4fd92997ef12bba14
rtpop <romanatab@hotmail.com>
2025-05-02 10:23:55 +0200

    fixed qscore outputting

--- bihidef/bihidef.py
+++ bihidef/bihidef.py
@@ -214,18 +214,25 @@ def run(filename, jaccard, resolution_graph, resolution_graphR, all_resolutions,
         nodename = '{:.4f}'.format(all_resolutions[i])
         resolution_graph.nodes[nodename]['matrix'] = results[i][0]
         resolution_graphR.nodes[nodename]['matrix'] = results[i][1]
-        qscore_tar = results[i][2]["tar_qscores"]
-        qscore_res = results[i][2]["reg_qscores"]
-        qscore_tar["resolution"] = all_resolutions[i]
-        qscore_res["resolution"] = all_resolutions[i]
+        if i == 0:
+            continue
+        else:  
+            qscore_tar = results[i][2]["tar_qscores"]
+            qscore_res = results[i][2]["reg_qscores"]
+            qscore_tar["resolution"] = all_resolutions[i]
+            qscore_res["resolution"] = all_resolutions[i]
+            qscores_tar.append(qscore_tar)
+            qscores_res.append(qscore_res)
+
         cluT.add_clusters(resolution_graph, all_resolutions[i])
         cluR.add_clusters(resolution_graphR, all_resolutions[i])
 
-        qscores_tar.append(qscore_tar)
-        qscores_res.append(qscore_res)
-
-        qscore_res.to_csv("qscore_reg.csv", index=False)
-        qscore_tar.to_csv("qscore_tar.csv", index=False)
+        
+    # remove reg_ and tar_ from the node names
+    qscore_tar[0] = qscore_tar[0].str.replace("tar_", "")
+    qscore_res[0] = qscore_res[0].str.replace("reg_", "")
+    qscore_res.to_csv("qscore_reg.csv", index=False)
+    qscore_tar.to_csv("qscore_tar.csv", index=False)
 
     # Return the cluster graphs and matrices for further analysis
     return cluT, cluR, gn, rg, A, B
