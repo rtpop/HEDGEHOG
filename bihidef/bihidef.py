7f7b7ced50b96d34394dfaa88c7e5a1e5eb13c0a
Romana Pop <romanatab@homail.com>
2026-04-17 08:24:51 +0200

    re-adding q-score calculation for testing

--- bihidef/bihidef.py
+++ bihidef/bihidef.py
@@ -109,8 +109,8 @@ def run_alg(condor_object, resolution, comm_mult):
     condor_object.brim(resolution=resolution, c = max_com)
 
     # calculate q scores
-    # condor_object.qscores(c = max_com, resolution=resolution)
-    # Qscores = condor_object.Qscores
+    condor_object.qscores(c = max_com, resolution=resolution)
+    Qscores = condor_object.Qscores
 
     # Extract unique community identifiers for target and regulator members and sort them
     clT = sorted(condor_object.tar_memb["community"].unique())
